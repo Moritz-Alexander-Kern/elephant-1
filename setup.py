3d34a504faffc47055924b1ddcf3f5fc527c5cdb
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-04-11 16:21:14 +0200

    remove fim module in setup.py

--- setup.py
+++ setup.py
@@ -17,48 +17,48 @@ for extra in ['extras', 'docs', 'tests', 'tutorials', 'cuda', 'opencl']:
     with open('requirements/requirements-{0}.txt'.format(extra)) as fp:
         extras_require[extra] = fp.read()
 
-# if platform.system() == "Windows":
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=[],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-fopenmp', '/std:c++17'])
-# elif platform.system() == "Darwin":
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=['pthread', 'omp'],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
-#             '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
-#             '-Xpreprocessor',
-#             '-fopenmp', '-std=gnu++17'])
-# else:
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=['pthread', 'gomp'],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
-#             '-Weffc++', '-Wunused-result', '-Werror',
-#             '-fopenmp', '-std=gnu++17'])
+if platform.system() == "Windows":
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=[],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-fopenmp', '/std:c++17'])
+elif platform.system() == "Darwin":
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=['pthread', 'omp'],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
+            '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
+            '-Xpreprocessor',
+            '-fopenmp', '-std=gnu++17'])
+else:
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=['pthread', 'gomp'],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
+            '-Weffc++', '-Wunused-result', '-Werror',
+            '-fopenmp', '-std=gnu++17'])
 
 setup(
     name="elephant",
     version=version,
     packages=['elephant', 'elephant.test'],
     include_package_data=True,
-    #ext_modules=[fim_module],
+    # ext_modules=[fim_module],
     install_requires=install_requires,
     extras_require=extras_require,
     author="Elephant authors and contributors",
