313b36f2c021d5e9bfcf72b8f6e70b6616915be6
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-04-12 15:51:18 +0200

    do not compile external modules for DARWIN

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
+import os.path
 import platform
 
 from setuptools import setup, Extension
-from distutils.version import LooseVersion
-from distutils.sysconfig import get_config_var
 
 print(platform.system())
 with open(os.path.join(os.path.dirname(__file__),
@@ -20,62 +18,62 @@ for extra in ['extras', 'docs', 'tests', 'tutorials', 'cuda', 'opencl']:
     with open('requirements/requirements-{0}.txt'.format(extra)) as fp:
         extras_require[extra] = fp.read()
 
-# if platform.system() == "Windows":
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         optional=True,
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=[],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-fopenmp', '/std:c++17'])
-# elif platform.system() == "Darwin":
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         optional=True,
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=['pthread', 'omp'],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
-#             '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
-#             '-Xpreprocessor',
-#             '-fopenmp', '-std=gnu++17'])
-# else:
-#     fim_module = Extension(
-#         name='elephant.spade_src.fim',
-#         optional=True,
-#         sources=['elephant/spade_src/src/fim.cpp'],
-#         include_dirs=['elephant/spade_src/include'],
-#         language='c++',
-#         libraries=['pthread', 'gomp'],
-#         extra_compile_args=[
-#             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
-#             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
-#             '-Weffc++', '-Wunused-result', '-Werror',
-#             '-fopenmp', '-std=gnu++17'])
+if platform.system() == "Windows":
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        optional=True,
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=[],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-fopenmp', '/std:c++17'])
+elif platform.system() == "Darwin":
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        optional=True,
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=['pthread', 'omp'],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
+            '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
+            '-Xpreprocessor',
+            '-fopenmp', '-std=gnu++17'])
+else:
+    fim_module = Extension(
+        name='elephant.spade_src.fim',
+        optional=True,
+        sources=['elephant/spade_src/src/fim.cpp'],
+        include_dirs=['elephant/spade_src/include'],
+        language='c++',
+        libraries=['pthread', 'gomp'],
+        extra_compile_args=[
+            '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
+            '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
+            '-Weffc++', '-Wunused-result', '-Werror',
+            '-fopenmp', '-std=gnu++17'])
 
-setup(
-    name="elephant",
-    version=version,
-    packages=['elephant', 'elephant.test'],
-    include_package_data=True,
-    # ext_modules=[fim_module],
-    install_requires=install_requires,
-    extras_require=extras_require,
-    author="Elephant authors and contributors",
-    author_email="contact@python-elephant.org",
-    description="Elephant is a package for analysis of electrophysiology data in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="BSD",
-    url='http://python-elephant.org',
-    python_requires=">=3.7",
-    classifiers=[
+setup_kwargs = {
+    "name": "elephant",
+    "version": version,
+    "packages": ['elephant', 'elephant.test'],
+    "include_package_data": True,
+    "ext_modules": [fim_module],
+    "install_requires": install_requires,
+    "extras_require": extras_require,
+    "author": "Elephant authors and contributors",
+    "author_email": "contact@python-elephant.org",
+    "description": "Elephant is a package for analysis of electrophysiology data in Python", # noqa
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "license": "BSD",
+    "url": 'http://python-elephant.org',
+    "python_requires": ">=3.7",
+    "classifiers": [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
@@ -88,4 +86,9 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering']
-)
+}
+# do not compile external modules on darwin
+if platform.system() == "Darwin":
+    del setup_kwargs["ext_modules"]
+
+setup(**setup_kwargs)
