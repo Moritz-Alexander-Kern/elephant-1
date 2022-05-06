7f452d33d0d6c35fd648dfd7502f6d1de0aa8d1a
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-05-06 23:47:35 +0200

    added -Xclang

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ elif platform.system() == "Darwin":
             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
             '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
-            '-Xpreprocessor',
+            '-Xpreprocessor', "-Xclang",
             '-fopenmp', '-std=gnu++17'],
         extra_link_args=['-lomp'])
 elif platform.system() == "Linux":
