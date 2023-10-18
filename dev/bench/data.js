ca1ec25bcd2d526f9618874ba713e4439ed0599e
github-action-benchmark <github@users.noreply.github.com>
2023-10-18 08:17:24 +0000

    add Benchmark (pytest) benchmark result for 99165ba2901e00f65c23c3cac92032cbf464da3f

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1697617044807,
+  "repoUrl": "https://github.com/Moritz-Alexander-Kern/elephant-1",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Moritz-Alexander-Kern",
+            "username": "Moritz-Alexander-Kern"
+          },
+          "committer": {
+            "name": "Moritz-Alexander-Kern",
+            "username": "Moritz-Alexander-Kern"
+          },
+          "id": "99165ba2901e00f65c23c3cac92032cbf464da3f",
+          "message": "Enh/benchmarks",
+          "timestamp": "2022-03-14T15:42:50Z",
+          "url": "https://github.com/Moritz-Alexander-Kern/elephant-1/pull/6/commits/99165ba2901e00f65c23c3cac92032cbf464da3f"
+        },
+        "date": 1697617043621,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "elephant/test/test_spike_train_surrogates.py::test_benchmark_surrogates",
+            "value": 393.84599272319514,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00038898181080066483",
+            "extra": "mean: 2.539063538734099 msec\nrounds: 284"
+          }
+        ]
+      }
+    ]
+  }
+}
