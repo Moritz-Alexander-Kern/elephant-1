00279db4f243601bb3b46cdb5780fe7ac3030bef
Moritz-Alexander-Kern <92092328+Moritz-Alexander-Kern@users.noreply.github.com>
2024-06-06 17:21:19 +0200

    test

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ elif platform.system() == "Darwin":
             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
             '-Weffc++', '-Wunused-result', '-Werror', '-Werror=return-type',
             '-Xpreprocessor',
-            '-fopenmp', '-std=c++17', '-fexperimental-library'],
+            '-fopenmp', '-std=c++17'],
         optional=False
     )
 elif platform.system() == "Linux":
